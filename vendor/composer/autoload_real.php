ad71ca3f344c293c47ca8b9fbf318da42dc33c79
Dais Nurfaizi <dsnurfaizi@gmail.com>
2021-02-21 11:04:21 +0700

    update composer

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInit083fb2d9680aa77d66c72192989c6645
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit083fb2d9680aa77d66c72192989c6645', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit083fb2d9680aa77d66c72192989c6645', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit083fb2d9680aa77d66c72192989c6645::getInitializer($loader));
         } else {
